313116999dc80dbb4b7706a4c0f6d03dd5a341d6
guilhermemellojw <guigomelo9@gmail.com>
2026-05-06 16:35:10 -0300

    fix(supervisor): fix syntax error in calculateAggregateSummary

--- app/src/main/java/com/antigravity/healthagent/ui/supervisor/SupervisorViewModel.kt
+++ app/src/main/java/com/antigravity/healthagent/ui/supervisor/SupervisorViewModel.kt
@@ -614,7 +614,7 @@ class SupervisorViewModel @Inject constructor(
                     }
                 }
             }
-        }     }
+        }
 
         return AggregateSummary(
             totalWorked = totalWorked,
